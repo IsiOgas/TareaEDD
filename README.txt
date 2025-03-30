bacce76705903d2ed72b2bab6223465b3daf7087
IsiOgas <isidoraogaspavez@gmail.com>
2025-03-30 19:01:04 -0300

    Se realiza la op 3.

--- README.txt
+++ README.txt
@@ -4,3 +4,8 @@ Rol: 202473654-K
 
 Nombre: Matilde Vásquez
 Rol: 202473652-3
+
+
+Importante antes de compilar, cuando se les pida un número cada uno va con una operación realizada. 
+Si se ingresa el número 0, entonces el programa hara una reflección horizontal
+Si se ingresa el número 3, entonces el programa les pedira el grado de atenuacion para cambiar la imagen a como lo deseen.
--- base.cpp
+++ base.cpp
@@ -27,6 +27,8 @@ void save(Imagen* img, const char* filename) {
     stbi_write_png(filename, img->width, img->height, img->channels, img->data, img->width * img->channels);
     cout << "Imagen guardada:  " << filename << "\n";
 }
+
+
 void operacion_1(Imagen* img){ //reflección horizontal
     for (int y = 0; y < img->height; y++) { //para recorrer fila
         for (int x = 0; x < img->width/2; x++){ //recorrer la mitad de las columnas porque asi el intercambio de pixeles se realizara 1 sola vez, si se recorriera toda la fila se haria 2 veces el cambio lo cual es innecesario
@@ -43,6 +45,17 @@ void operacion_1(Imagen* img){ //reflección horizontal
                 img->data[pixel_derecho + c] = tmp; //Se asigna el valor del pixel izq al derecho
             }
         }
+    }
+}
+
+void operacion_3(Imagen* img, float atenuacion){ //Creamos una función void que no devuelve ningun valor solo modifica, y agregamos nuestro puntero y la funcion float.
+    if (atenuacion < 0.0f || atenuacion > 1.0f){ //Revisamos si el valor dado esta dentro del rango [0,1] además usamos ese f que se refiere a un núm float.
+        cout <<"EL valor ingresado debe estar entre 0.0 y 1.0.\n"; //Si el valor dado cumple la condición de arriba arroja este error.
+        return;
+    }
+
+    for(int i = 0; i < img->width * img->height * img->channels; i++){ //Si el valor dado esta dentro del rango,  recorremos  el arreglo. además cada pixel lo guardamos en la posición i.
+        img->data[i] = static_cast<unsigned char>(img->data[i] * atenuacion); //Acá multiplicamos cada pixel por el grado de atenuación. Esta operación como que "oscurese o hace mas transparente" y luego lo convertimos a usigned char para asegurar que esta en el rango valido para img.
     }
 }
 
@@ -63,9 +76,16 @@ int main() {
     //img->data[101] = 0;
 
     if (numero == 0 ){
-
         operacion_1(img);
     }
+
+    if (numero == 3){ //si el numero es 3 entonces entramos a esta función. #Leer README C:.
+        float atenuacion; //Declaramos la variable atenuacion que es de tipo float.
+        cout<<"Ingrese un grado de atenuación entre 0.0 y 1.0: "; //El usario nos otorga un valor que hará que la imagen se atenue.
+        cin >> atenuacion;//El valor asignado por el usario se guarda en la variable atenuacion.
+        operacion_3(img, atenuacion);//Llamamos a la función que es la que modifica el grado de atenuacion.
+    }
+
     // Almacenamos el resultado
     save(img, "out.png");
 }

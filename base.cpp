0a8fcc1f71a877314a8b3509e70b20bf7a331034
mtmzdoz <mmvasquezro@gmail.com>
2025-03-29 22:41:47 -0300

    Intento Op1

--- base.cpp
+++ base.cpp
@@ -10,15 +10,16 @@ using namespace std;
 
 // Struct que almacena la data de una imagen
 struct Imagen{
-    unsigned char* data;
-    int width, height, channels;
+    unsigned char* data; // Apunta los datos de la imagen
+    int width, height, channels; // Como imagen png, cada pixel tiene 3 canales RGB
 };
 
 // Abrir imagen desde memoria local
+//img->data, es acceder a los atributos. Puntero img, accede a los atributos data.
 Imagen* load(const char* filename) {
-    Imagen* img = new Imagen();
-    img->data = stbi_load(filename, &img->width, &img->height, &img->channels, 0);
-    return img;
+    Imagen* img = new Imagen(); //se crea "nueva" Imagen con el puntero img
+    img->data = stbi_load(filename, &img->width, &img->height, &img->channels, 0); //stbi load carga la info de la imagen en la direccion  correspondiente de cada uno de ancho, alto y canal
+    return img; //devuelve el puntero
 }
 
 // Guardar imagen en memoria local
@@ -26,19 +27,44 @@ void save(Imagen* img, const char* filename) {
     stbi_write_png(filename, img->width, img->height, img->channels, img->data, img->width * img->channels);
     cout << "Imagen guardada:  " << filename << "\n";
 }
+void operacion_1(Imagen* img){ //reflección horizontal
+    for (int y = 0; y < img->height; y++) { //para recorrer fila
+        for (int x = 0; x < img->width/2; x++){ //recorrer la mitad de las columnas porque asi el intercambio de pixeles se realizara 1 sola vez, si se recorriera toda la fila se haria 2 veces el cambio lo cual es innecesario
+            //La fórmula para obtener la posición de un píxel en un arreglo unidimensional
+            //index = (fila * ancho + columna) * canales
+            int pixel_izquierdo = (y * img->width + x) * img->channels; //pos 0, al *channels nos aseguramos de que tambien estamos considerando los canales RGB
+            int pixel_derecho = (y * img->width + (img->width - x - 1)) * img->channels; //pos -1
+
+            //para recorrer los canales
+            for (int c = 0; c < img->channels; c++) { 
+                unsigned char tmp = img->data[pixel_izquierdo + c]; //Se guarda el valor del canal
+                img->data[pixel_izquierdo + c] = img->data[pixel_derecho + c]; //Se asigna el canal del pixel derecho al izq
+                img->data[pixel_derecho + c] = tmp; //Se asigna el valor del pixel izq al derecho
+            }
+        }
+    }
+}
+
 
 int main() {
+    
+    int numero;
+    cout << "Ingrese un número: "<< endl;
+    cin >> numero;
+    
+        
+    
+
     //Cargamos la imagen
     Imagen* img = load("Pikachu.png");
 
     //Modificamos un par de pixeles en el borde superior de la imagen como ejemplo
-    img->data[101] = 0;
-    img->data[102] = 0;
-    img->data[103] = 0;
-    img->data[104] = 0;
-    img->data[105] = 0;
-    img->data[106] = 0;
+    //img->data[101] = 0;
 
+    if (numero == 0 ){
+
+        operacion_1(img);
+    }
     // Almacenamos el resultado
-    save(img, "output.png");
+    save(img, "out.png");
 }
